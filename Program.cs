cc1bfc66359fac01f9b6b8ca0a72c82a5dd607f5
Sillver Bullet <161559713+leesuper-learn@users.noreply.github.com>
2024-02-28 16:48:43 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap1_w02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten tu ban phim: ");
+            string str_name = Console.ReadLine();
+            Console.WriteLine("Xin chao " + str_name);
+
+            Console.ReadKey();
+        }
+    }
+}
